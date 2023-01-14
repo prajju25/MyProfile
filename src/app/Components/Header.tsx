43246a3a9cbed67aeb320a902af97a0101f545a4
prajju25 <kotian.prajwal821@gmail.com>
2023-01-14 15:05:05 +0530

    style changes

--- src/app/Components/Header.tsx
+++ src/app/Components/Header.tsx
@@ -2,19 +2,37 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 import { Icon, Menu } from 'semantic-ui-react';
 
-const Header = ({displaySideMenu, menus, isSidebar}:any) => {
+const Header = ({displaySideMenu, isSidebar}:any) => {
     return (
         <Menu style={{ marginTop: '10px'}} className="container-bg">
             <a className='item' onClick={()=>displaySideMenu(!isSidebar)}>
                 <Icon disabled name='th' />
             </a>
             <Menu.Menu position='right'>
-                {menus.map((m: { menuItem: string, to: string }, k: number)=>{
-                    return (
-                        <Link to={m.to} className='item'
-                        key={m.menuItem+k}>{m.menuItem}</Link>
-                    );
-                })}
+                <div className='item'>
+                    Contact:
+                </div>
+                <div className='item'>
+                    <a href='https://twitter.com/prajju2511'>
+                        <Icon circular inverted name="twitter" color='blue'/>
+                    </a>
+                </div>
+                <div className='item'>
+                    <a href='https://www.facebook.com/prajwal.kotian'>
+                        <Icon circular inverted name="facebook" color='blue'/>
+                    </a>
+                </div>
+                <div className='item'>
+                    <a href='https://www.linkedin.com/in/prajwal-kotian-4ba48980/'>
+                        <Icon circular inverted name="linkedin" color='blue'/>
+                    </a>
+                </div>
+                <div className='item'>
+                    <a href='https://www.instagram.com/prajju25/'>
+                        <img height={25}
+                        src="https://s.yimg.com/fz/api/res/1.2/7wmcqjtgVFiMeMxLOlW3sw--~C/YXBwaWQ9c3JjaGRkO2ZpPWZpdDtoPTI0MDtxPTgwO3c9MjQw/https://s.yimg.com/zb/imgv1/e9d6381f-6a16-39d9-b6f9-1467838c022c/t_500x300" />
+                    </a>
+                </div>
             </Menu.Menu>
         </Menu>
     );
--- src/app/Components/Layout.tsx
+++ src/app/Components/Layout.tsx
@@ -1,12 +1,9 @@
 import React, { useState } from "react";
-import { useLocation } from "react-router-dom";
-import { Breadcrumb, Container, Grid, Menu, Segment, Sidebar } from "semantic-ui-react";
+import { Container, Grid, Menu, Segment, Sidebar } from "semantic-ui-react";
 import Header from "./Header";
 import SideMenu from "./SideMenu";
 
 const Layout = (props: any) => {
-    const location = useLocation();
-    let isActive = ['/profile','/photography','/apps'].includes(location.pathname);
     const [state, setState] = useState({
         displaySideMenu: false
     });    
@@ -22,19 +19,8 @@ const Layout = (props: any) => {
     }
     return (
         <Container>
-            <Header displaySideMenu={displaySideMenu} menus={menus} isSidebar={state.displaySideMenu}/>
+            <Header displaySideMenu={displaySideMenu} isSidebar={state.displaySideMenu}/>
             <Grid>
-                <Grid.Row className="breadcrumbs-row">
-                    <Grid.Column className="container-bg breadcrumbs">
-                        <Breadcrumb>
-                            <Breadcrumb.Section href='/' link={isActive} active={!isActive}>Home</Breadcrumb.Section>
-                            {isActive && (<>
-                            <Breadcrumb.Divider />
-                            <Breadcrumb.Section active>{menus.find(m=>m.to == location.pathname)?.menuItem}</Breadcrumb.Section>
-                            </>)}
-                        </Breadcrumb>
-                    </Grid.Column>
-                </Grid.Row>
                 <Grid.Row>
                     <Grid.Column>
                         <Sidebar.Pushable as={Segment}>
