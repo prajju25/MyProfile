3e90538657d5a912ea9437308767adfb1a618e6f
prajju25 <kotian.prajwal821@gmail.com>
2024-10-30 19:18:47 +0530

    netlify function response fix

--- netlify/functions/verifyToken.js
+++ netlify/functions/verifyToken.js
@@ -32,7 +32,7 @@ exports.handler = async (event, context) => {
 
     return {
       statusCode: 200,
-      body: { response: data },
+      body: JSON.stringify(data),
     };
   } catch (error) {
     console.error("Error exchanging token:", error);
